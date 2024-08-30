0d96d760b14f61b133568bd66c10a4677c29f1af
hung <hunglt.ee@gmail.com>
2024-08-30 21:30:16 +0700

    do it

--- change_type.js
+++ change_type.js
@@ -3,26 +3,30 @@ const fs = require('fs');
 function injectpng(filename, callback) {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, (err, data) => {
-      if(err){
-        reject(err)
+      if (err) {
+        return reject(err);
       }
       fs.readFile('base.png', (err, image_buffer) => {
-        if(err){
-          reject(err)
+        if (err) {
+          return reject(err);
         }
-        const new_file = filename.replace('.ts', '.png')
-        fs.writeFile(new_file, Buffer.from(Buffer.concat([image_buffer, data], 'binary'), (err, info) => {
-          if(err){
-            reject(err)
+        const new_file = filename.replace('.ts', '.png');
+        const combinedBuffer = Buffer.concat([image_buffer, data]);
+        fs.writeFile(new_file, Buffer.from(combinedBuffer), (err) => {
+          if (err) {
+            return reject(err);
           }
-          fs.unlink(filename, (err, _) => {
-            if(err){
-              reject(err)
+          fs.unlink(filename, (err) => {
+            if (err) {
+              return reject(err);
             }
             return resolve(new_file);
-          })
-        }))
-      })
-    })
-  })
+          });
+        });
+      });
+    });
+  });
 }
+
+injectpng('sample_640x360.ts', ()=>{});
+// node change_type.js
